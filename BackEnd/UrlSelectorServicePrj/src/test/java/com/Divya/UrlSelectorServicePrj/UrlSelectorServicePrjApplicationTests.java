5f8b2334884a521396e59682cf2339ba16efac96
DivyaV10 <divyavacharya94@gmail.com>
2018-09-27 07:13:38 +0530

    SpringBoot

---
+++ BackEnd/UrlSelectorServicePrj/src/test/java/com/Divya/UrlSelectorServicePrj/UrlSelectorServicePrjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Divya.UrlSelectorServicePrj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UrlSelectorServicePrjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
